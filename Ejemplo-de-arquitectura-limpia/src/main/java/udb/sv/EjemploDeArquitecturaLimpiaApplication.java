6036aae5c8a6e253401310c5e783b79ed6f98dca
dCartaArt02 <ca241136@alumno.udb.edu.sv>
2025-08-06 20:09:40 -0600

    Proyecto inicializado en spring initializr

---
+++ Ejemplo-de-arquitectura-limpia/src/main/java/udb/sv/EjemploDeArquitecturaLimpiaApplication.java
@@ -0,0 +1,13 @@
+package udb.sv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploDeArquitecturaLimpiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploDeArquitecturaLimpiaApplication.class, args);
+	}
+
+}
---
+++ Ejemplo-de-arquitectura-limpia/src/test/java/udb/sv/EjemploDeArquitecturaLimpiaApplicationTests.java
@@ -0,0 +1,13 @@
+package udb.sv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploDeArquitecturaLimpiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
